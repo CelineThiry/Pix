50878341c7228c202d0a1fbea10a39e17eb5e076
Céline Thiry <thiry.celi@gmail.com>
2016-04-03 20:39:47 +0200

    First stone

---
+++ index-2.php
@@ -0,0 +1,47 @@
+<?php
+
+include('./nav.php');
+
+?>
+	
+		<aside>		
+		
+		
+		<figure>
+			<img src="img/images/design_homepage2_01.jpg" alt="">
+			<a href=""></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage2_02.jpg" alt="">
+			<a href="services/design.html" class="discutons-en button"></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage2_03.jpg" alt="">
+			<a class="" href=""></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage2_04.jpg" alt="">
+			<a href="index-2.html" class="next arrow"></a>
+			<a href="index.html" class="previous arrow"></a>
+			<a href="services/design.html" class="identity button"></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage2_05.jpg" alt="">
+			<a href="index.html" class="first button"></a>
+			<a href="index-2.html" class="second button"></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage2_06.jpg" alt="">
+			
+		</figure>
+		
+		</aside>
+	
+</section>
+
+	
+
+
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+include('./nav.php');
+
+
+?>
+	
+		<aside>
+		<figure>
+			<img src="img/images/design_homepage_01.jpg" alt="">
+			<a href=""></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage_02.jpg" alt="">
+			<a href="contact.html" class="discutons-en button"></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage_03.jpg" alt="">
+			<a class="" href=""></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage_04.jpg" alt="">
+			<a href="index-2.html" class="next arrow"></a>
+			<a href="index.html" class="previous arrow"></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage_05.jpg" alt="">
+			<a href=""></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage_06.jpg" alt="">
+			<a href="index.html" class="first button"></a>
+			<a href="index-2.html" class="second button"></a>
+		</figure>
+		<figure>
+			<img src="img/images/design_homepage_07.jpg" alt="">
+			<a href=""></a>
+		</figure>	
+		
+
+		</aside>
+	
+</section>
+
+	
+
+
+	
+</body>
+</html>
