424f00e0e1f36704e4aaf1740e42959ad5105905
Céline Thiry <thiry.celi@gmail.com>
2016-04-05 16:59:10 +0200

    c

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$(".trigger").click(function(){
 		//slide up all the link lists
+		$(this).toggleClass("active");
 		$(".dropdown").slideUp();
 		//slide down the link list below the .trigger clicked - only if its closed
 		if(!$(this).next().is(":visible"))
