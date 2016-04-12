0edeea056e075899ef371b28d67221431f33655c
Céline Thiry <thiry.celi@gmail.com>
2016-04-12 12:36:36 +0200

    - Changed animation on socila media on sidebar - Changed text-align on with media

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+	<!-- I FOUND SOMETHING IMPORTANT : Tag above is what define how the design must reacts to different size of device such as tablet and smartphone  -->
+	<meta charset="UTF-8">
+	<title>Pixinko</title>
+	<meta name="description" content="Pixinko, votre agence crehacktive 360°.">
+	<link rel="stylesheet" href="css/foundation.css">	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/slider.css">
+	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Unica+One' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<section class="container">
+	<div class="sidebar">
+		<header>
+			<a href="index.html">
+			<img src="img/logo_pixinko.svg" alt="">
+		</a>
+		</header>
+		<nav>	
+			<ul class="my-menu">
+				<li><a href="#">Agence</a></li>
+				<li><a href="#">Réalisations</a></li>
+				<li>
+				<a href="#">Services</a><button class="trigger active"><i class="fa fa-chevron-down"></i></button>
+					<ul class="dropdown my-submenu">
+						<li><a href="#">Web et référencement</a></li>
+						<li><a href="#">Design</a></li>
+						<li><a href="#">Marketing digital</a></li>
+						<li><a href="#">Hébergement et cloud</a></li>
+						<li><a href="#">Gestion réseaux</a></li>
+						<li><a href="#">Application mobile</a></li>
+						<li><a href="#">Vidéos</a></li>
+						<li><a href="#">Conseils et formations</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Actualités</a></li>
+				<li><a href="#">Contact</a></li>
+				
+			</ul>		
+		</nav>
+		<footer>
+			<div class="address">
+				<p>+32 499 11 89 62</p>
+				<p>info(at)pixinko.be</p>			
+			</div>
+			
+			<ul class="social-media">
+				<li>
+					<a class="ease-all facebook" href="#">
+						<i class="fa fa-facebook"></i>
+						<span class="hide-text">Facebook</span>
+					</a>
+				</li>
+				<li>
+					 <a class="ease-all twitter" href="#">
+					 <i class="fa fa-twitter"></i>
+					 <span class="hide-text">Twitter</span>
+					 </a>
+				</li>
+				<li>
+					<a class="ease-all linkedin" href="#">
+					 <i class="fa fa-linkedin"></i>
+					 <span class="hide-text">Linkedin</span>
+					 </a>
+				</li>
+				
+			</ul>
+			<div class="copyright">
+				<p>© 2016 Pixinko</p>
+			</div>
+			
+		</footer>
+	</div> <!-- END SIDEBAR -->
+	
+	<div class="menu-responsive">
+		<div class="burger-icon-container">			
+			<div class="burger">
+				<div class="icon"></div>	
+			</div>				
+		</div>
+		
+			<a class="" href="index.html">
+				<img src="img/logo_pixinko.svg" alt="">
+			</a>
+		
+		 
+	</div><!-- END RESPONSIVE MENU -->
+	<!-- <header class="menu-responsive">
+		<div class="my-header row align-middle">
+			<div class="home-logo small-9 columns">
+				<a href="index.html"><img src="img/logo_pixinko.svg" alt="Home"></a>
+			</div>
+			<div class="burger-icon-container columns">
+				
+					<div class="burger align-middle">
+						<div class="icon"></div>	
+					</div>
+				
+			</div>			
+		</div>	
+		<div class="navigation">			
+				<nav class="navigation-list">	
+					<ul class="my-menu responsive">
+						<li><a href="#">Agence</a></li>
+						<li><a href="#">Réalisations</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+					<footer>
+						<ul class="social-media responsive">
+							<li><a href=""><img src="img/linkedin.svg" alt="linkedin"></a></li>
+							<li><a href=""><img src="img/twitter.svg" alt="twitter"></a></li>
+							<li><a href=""><img src="img/facebook.svg" alt="facebook"></a></li>
+						</ul>
+						<div class="address responsive">
+							<p>+32 499 11 89 62</p>
+							<p>info(at)pixinko.be</p>			
+						</div>
+						<div class="copyright responsive">
+							<p>c 2016 Pixinko</p>
+						</div>
+					</footer>
+				</nav> <!-- END NAV-LIST
+		</div><!-- END NAVIGATION -->		
+	<!-- </header> -->
+		<!-- END HEADER -> ONLY VISIBLE ON TABLET AND SMARTPHONE -->
+
+	<main>
+		<div class="container-cover layer1">
+			<div class="slider">
+			  <ul>
+			    <li id="slide1">
+			      <img src="img/slide1.jpg" />
+			    </li>
+			    <li id="slide2">
+			      <img src="img/slide2.jpg" />
+			    </li>
+			    <li id="slide3">
+			      <img src="img/slide3.jpg" />
+			    </li>
+			  </ul>
+			  	
+			</div> <!-- END SLIDER -->			
+		</div><!-- END CONTAINER / LAYER1-->
+		<div class="layer2">
+			<div class="wrapper">
+				<div class="second-wrapper">
+					<button class="prev"><i class="fa fa-chevron-right"></i></button>
+		  			<button class="next"><i class="fa fa-chevron-left"></i></button>
+				</div>
+				<div class="second-wrapper bullet">
+					<ul>
+						<li class="active"><a href="#" class="bullet1"></a></li>
+						<li><a href="#" class="bullet2"></a></li>
+						<li><a href="#" class="bullet3"></a></li>
+					</ul>
+				</div>
+			</div>				
+		</div> <!-- END LAYER 2 -->
+		<div class="layer3">
+			<div class="slider-text">
+			  <ul>
+			    <li id="slide1">
+			      <div class="block-text">
+			      <h1>crehacktive<br>developpement<br>agency</h1>
+			      <p>print - web - mobile - video - training - IT</p>
+				      <div class="exergue">
+				      	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt cum provident</p>
+				      	
+				      </div>
+			      </div>
+			    </li>
+			    <li id="slide2">
+			      <div class="block-text">
+			      <h1>crehacktive<br>developpement<br>agency</h1>
+			      <p>print - web - mobile - video - training - IT</p>
+			      <div class="exergue">
+			      	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus error pariatur </p>
+			      	
+			      </div>
+
+			      </div>
+			    </li>
+			    <li id="slide3">
+			      <div class="block-text">
+			      <h1>crehacktive<br>developpement<br>agency</h1>
+			      <p>print - web - mobile - video - training - IT</p>
+				      <div class="exergue">
+				      	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+
+				      </div>
+			      </div>
+			    </li>
+			  </ul>
+			  	
+			</div> 			
+		</div>
+		<!-- END LAYER 3 -->
+	</main>
+
+</section> <!-- END CONTAINER -->
+
+
+
+<script src="js/jquery-1.12.2.min.js"></script>
+<script src="js/script.js"></script>
+<script src="js/slider.js"></script>
+<script src="js/vendor/foundation.min.js"></script>
+<script src="js/vendor/what-input.js"></script>
+	
+</body>
+</html>
